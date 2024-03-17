af190a40a9c762c30298aab0a39afd9e3d45bc67
benmcsausage <benjaminsaurage@yahoo.com>
2024-03-17 19:39:45 -0400

    added formating

--- WarcraftML/class1.py
+++ WarcraftML/class1.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import matplotlib.pyplot as plt
 
 clientId = "9b8b018d-77bc-4af6-8965-cc422d67be58"
 secretId= "G4VH4kaXL1UCat7mp3Uxjy26UJ76fRK5meJ7Rc46"
@@ -106,8 +107,7 @@ def format_raid_report(response):
              print(f"- Boss Kill: {fight['name']} (Fight ID: {fight['id']})")
         elif fight["name"] in boss_encounters:
             print(f"- Boss Kill: {fight['name']} (Fight ID: {fight['id']})")
-        else:
-            print(f"- Non-Boss Fight: {fight['name']} (Fight ID: {fight['id']})")
+
 def get_data(query: str, **kwargs):
     """Fetches data from wclogs public API. Please provide a query and the parameters."""
     data = {"query": query, "variables": kwargs}
@@ -121,7 +121,43 @@ def get_data(query: str, **kwargs):
         else:
             print(f"Request failed with status code {response.status_code}")
             return None
+"""def count_wipes_per_boss(response):
+    wipes_count = {}
 
+    if "errors" in response:
+        print("Error:", response["errors"])
+        return wipes_count
+    
+    report = response["data"]["reportData"]["report"]
+    
+    for fight in report["fights"]:
+        if fight.get("boss"):  # Check if it's a boss fight
+            boss_name = fight["name"]
+            if boss_name not in wipes_count:
+                wipes_count[boss_name] = 0
+            
+            for event in fight["events"]:
+                # Check for events indicating a wipe
+                if event["type"] == "death" and event["sourceID"] in report["friendlies"]:
+                    # If a player died, check if it's a group wipe
+                    if all(death["type"] == "wipe" for death in event["additionalDeaths"]):
+                        wipes_count[boss_name] += 1
+                        
+    return wipes_count
+
+def plot_wipes_per_boss(wipes_count):
+    bosses = list(wipes_count.keys())
+    wipes = list(wipes_count.values())
+    print("Bosses:", bosses)
+    print("Wipes:", wipes)v
+    plt.figure(figsize=(10, 6))
+    plt.bar(bosses, wipes, color='skyblue')
+    plt.xlabel('Boss')
+    plt.ylabel('Number of Wipes')
+    plt.title('Number of Wipes for Each Boss Encounter')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()"""
 def main():
     response = get_token()
     store_token(response)
@@ -132,4 +168,6 @@ def main():
     #print(response1)
     raid_report_response = get_raid_report("2RngATHaBQpd3Y4b")
     format_raid_report(raid_report_response)
+    #wipes_count_per_boss = count_wipes_per_boss(raid_report_response)
+    #plot_wipes_per_boss(wipes_count_per_boss)
 if __name__ == "__main__": main()
